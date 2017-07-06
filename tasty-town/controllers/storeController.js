dc6678bbad05d8fa66987808c5b1a81e1ce0fbd5
Derrick <stjohndlee@gmail.com>
2017-07-07 00:40:42 +0900

    added geolocation

--- tasty-town/controllers/storeController.js
+++ tasty-town/controllers/storeController.js
@@ -31,6 +31,10 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  // set the location data to be a Point
+  // console.log(req.body);
+  req.body.location.type = 'Point';
+
   const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
     new: true, // return the new store instead of the old one
     runValidators: true, // force the model to run validators
