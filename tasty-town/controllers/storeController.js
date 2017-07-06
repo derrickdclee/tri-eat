c1d41d01557df854d574418e9e0a791f9e911ab8
Derrick <stjohndlee@gmail.com>
2017-07-06 23:15:52 +0900

    editing stores

--- tasty-town/controllers/storeController.js
+++ tasty-town/controllers/storeController.js
@@ -9,9 +9,33 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'});
 };
 
-
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  /*
+    flash only works if we are using Sessions
+    that's the only way data could persist from one request to the next
+  */
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // Query the db for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({_id: req.params.id});
+  res.render('editStore', {title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true, // force the model to run validators
+  }).exec(); // used in place of a callback function
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
