fd67b03898303acc5fdf7a66a1ef7c3507842be1
Derrick <stjohndlee@gmail.com>
2017-07-07 23:12:40 +0900

    added a page to view store by tag

--- tasty-town/controllers/storeController.js
+++ tasty-town/controllers/storeController.js
@@ -78,3 +78,15 @@ exports.updateStore = async (req, res) => {
   req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({slug: req.params.slug});
+  if (! store) return next(); // let the error handlers handle it
+  res.render('store', {store, title: store.name});
+};
+
+exports.getStoresByTag = async(req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', {tags, title: 'Tags List', tag});
+};
