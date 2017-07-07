e9d2a7dd37c8ba1242b2092a3267d182159197a5
Derrick <stjohndlee@gmail.com>
2017-07-07 16:31:38 +0900

    added image upload

--- tasty-town/controllers/storeController.js
+++ tasty-town/controllers/storeController.js
@@ -1,12 +1,47 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter: function(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true); // yah we good, proceed
+    } else {
+      next({message: 'That filetype isn`t allowed!'}, false);
+    }
+  }
+};
+const jimp = require('jimp');
+const uuid = require('uuid');
+
 const Store = mongoose.model('Store'); // from start.js
 
+//===========================================================================//
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'});
+};
+
+// upload to memory
+exports.upload = multer(multerOptions).single('photo');
+exports.resize = async (req, res, next) => {
+  console.log(req.body);
+  // check if there is no new file to resize
+  if (! req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we can resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO); // width is 800
+  // save to disk
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 };
 
 exports.createStore = async (req, res) => {
