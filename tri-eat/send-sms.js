8cb7845f05f37c38eb63713842db903f039557f3
Derrick <stjohndlee@gmail.com>
2017-08-04 02:23:47 +0900

    updated send-sms.js

--- tri-eat/send-sms.js
+++ tri-eat/send-sms.js
@@ -1,15 +1,15 @@
 
 // Twilio Credentials
-var accountSid = 'AC23e54e50f9c52838de8fa83a5b7c4656';
-var authToken = 'b5634d27cc9f982e7c8a7574127e1b11';
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
 
 //require the Twilio module and create a REST client
-var client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
-    to: "+821044430153",
+    to: "+15719267772",
     from: "+12156080971",
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+    body: "Hello baby!",
 }, function(err, message) {
     console.log(message.sid);
 });
